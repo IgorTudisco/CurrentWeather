42aed3360a3d04c9da25005f12b20124300da345
Igor Tudisco <igortudisco@hotmail.com>
2025-04-06 12:48:52 -0300

    Starting the project

---
+++ CurrentWeather/CurrentWeather/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CurrentWeather
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CurrentWeather/CurrentWeather/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CurrentWeather
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
